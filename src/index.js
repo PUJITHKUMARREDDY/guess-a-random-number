215f47dbb44af46e79a62b2e78efedff5404efdf
PUJITHKUMARREDDY <pujithkalluru@gmail.com>
2024-10-05 11:56:01 +0530

    Updated UI

--- src/index.js
+++ src/index.js
@@ -3,68 +3,103 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
 
+const mstyle  = 
+{
+  fontSize : "2rem"
+}
 const customButton =
 {
    color : "white",
    backgroundColor : "brown",
-   display : "block",
+   display: "inline-block",
    padding : "10px",
    fontSize : "20px",
    cursor : "pointer",
-   marginLeft : "auto",
+   marginLeft : "10px",
    marginRight : "auto",
    marginTop : "30px",
    borderRadius : "20px"
 
 }
 function generateId() {
-  return Math.floor(Math.random() * 15) + 1;
+  return Math.floor(Math.random() * 30) + 1;
 }
 
 function Guess() {
+  const [attempts,SetAttempts] = useState(0);
   const inputClear = document.getElementById("inputReset")
   const [input, setInput] = useState("");
   const [id,setId] = useState(generateId());
   const [message,setMessage] = useState("");
+  const [score,setScore] = useState(0);
   const handleGuess = () => {
+      if(input.length === 0)
+      {
+        alert("Please Enter a number");
+      }
+      else
+      {
         if (Number(input) === id) {
-       setMessage("Hurray!You Guessed the Number ");
-       inputClear.value = "";
-       
-      }
-      else if (input > id) {
-       setMessage("Expecting a lower Number");
-       inputClear.value = "";
-      }
-      else {
-        setMessage("Expecting a bigger Number"); 
-        inputClear.value = "";
+          setMessage("Hurray!You Guessed the Number ");
+          SetAttempts(attempts+1);
+          setScore(score=>score+10);
+          inputClear.value = "";
+          
+         }
+         else if (input > id) {
+          setMessage("Expecting a lower Number");
+          inputClear.value = "";
+          SetAttempts(attempts+1);
+         }
+         else {
+           setMessage("Expecting a bigger Number"); 
+           inputClear.value = "";
+           SetAttempts(attempts+1);
+         }
       }
   };
-  const playAgain = () =>
+  const PlayAgain = () =>
   {
+    
+    SetAttempts(0);
     setId(generateId());
     setInput("");
-    handleGuess();
   }
+  const reset = () =>
+  {
+    setScore(0);
+    SetAttempts(0);
+    setInput("");
+ }
   
   return (
     <>
-      <h1>Enter a number from 1 to 15</h1>
-      <input 
-      type="number"
-      min={1}
-      max={15}
-        id = "inputReset"
-        className='uiDesign'
-        onChange={(e) => setInput(e.target.value)}
-        placeholder='Enter a number'>
-      </input>
+    <p className='designLeftText'>Score:</p>
+      <div className='leftAlign'>
+        <h3>{score}</h3>
+      </div>
+      
+      <p className='designText'>Attempts:</p>
+      <div className='attempts'>
+          <h3>{attempts}</h3>
+      </div>
+          <div className='alignInput'>
+          <p style={mstyle}>Enter a number from 1 to 30</p>
+          <input 
+              type="number"
+              min={1}
+              max={30}
+              id = "inputReset"
+              className='uiDesign'
+              onChange={(e) => setInput(e.target.value)}
+              placeholder='Enter a number'>
+          </input>
+      </div>
+      
       <h3>{message}</h3>
       <button style = {customButton}onClick={handleGuess}>Submit</button>
-      <button style={customButton} onClick={playAgain}>Play Again</button>
-      
-
+      <button style={customButton} onClick={PlayAgain}>Play Again</button>
+      <button style={customButton} onClick={reset}>Reset</button>
     </>
 
   )
